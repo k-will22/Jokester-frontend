a61d63ca374e629452906862a31cb33299a8840f
k-will22 <thestreetkw@hotmail.com>
2021-01-22 14:17:34 -0500

    audio player attempt

--- my-app/src/components/JokeItem.js
+++ my-app/src/components/JokeItem.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import ReactAudioPlayer from 'react-audio-player';
 
 function JokeItem () {
+
+    function handleLaugh() {
+        return (
+            <ReactAudioPlayer
+                src="comic007.mp3"
+                autoPlay
+                controls
+            />
+        )
+    }
+
     return (
         <div>
         <h1>Jokes</h1>
@@ -9,6 +21,12 @@ function JokeItem () {
             <li>Joke 2</li>
             <li>Joke 3</li>
         </ul>
+        <button onClick={handleLaugh}>Laugh</button>
+        <ReactAudioPlayer
+                src="comic007.mp3"
+                autoPlay
+                controls
+            />
         </div>
     )
 }
